ff9d3cf7717dc6bd79e08e82329d300d48e25163
karim-id3m <karim.ebrahim@id3m.com>
2025-09-04 15:50:31 +0300

    vid-1:27 (authontication is don)

--- components/Login.vue
+++ components/Login.vue
@@ -11,6 +11,26 @@
         error=""
       />
     </div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Password"
+        v-model:input="password"
+        inputType="password"
+      />
+    </div>
+    <div class="px-6 text-[12px] text-gray-600">Forget password</div>
+    <div class="px-6 pb-2 mt-6">
+      <button
+        :disabled="!email || !password"
+        :class="(!email || !password)? 'bg-gray-200' : 'bg-[#f02c56]'"
+        @click="Login()"
+        class="w-full text-[17px] font-semibold text-white py-3 rounded-sm"
+      >Log in</button>
+    </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+let email = ref(null)
+let password = ref(null)
+let errors = ref(null)
+</script>
--- components/TextInput.vue
+++ components/TextInput.vue
@@ -3,12 +3,27 @@
     <input
       :id="`input-${placeholder}`"
       :placeholder="placeholder"
+      class="block w-full bg-[#f1f1f2] text-gray-800 border border-gray-300 rounded-md py-2.5 px-3 focus:outline-none"
       :type="inputType"
       v-model="inputComputed"
       autocomplete="off"
       :maxlength="max"
-      class="block w-full bg-[#f1f1f2] text-gray-300 border border-gray-300 rounded-md py-2.5 px-3 focus:outline-none"
     />
+    <span v-if="error" class="text-red-500 text-[14px] font-semibold">{{ error }}</span>
   </div>
 </template>
-<script setup></script>
+<script setup>
+const emit = defineEmits(['update:input'])
+const props = defineProps(['input','placeholder','inputType','max','autoFocus','error'])
+const {input,placeholder,inputType,max,autoFocus,error} = toRefs(props)
+
+onMounted(()=>{
+  if(autoFocus.value){
+    document.getElementById(`input-${placeholder.value}`).focus()
+  }
+});
+const inputComputed = computed({
+  get:()=>input.value,
+  set:(val)=>emit('update:input',val)
+});
+</script>
